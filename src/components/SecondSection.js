49b90af7b2313d1f014767660bb87d14ac1d6923
Shahinur-Rahman71 <shanto16026@gmail.com>
2022-09-04 23:14:06 +0600

    update

--- src/components/SecondSection.js
+++ src/components/SecondSection.js
@@ -136,16 +136,21 @@ function SecondSection() {
 
                         <div className='last_div'>
                             <div className='inner_lastdiv'>
-                                <Form.Check 
-                                    defaultChecked
-                                    type={'checkbox'}
-                                    id={`ac}`}
-                                    label={`I want to avail`}
-                                />
-                                <span>Insurance Service</span>
-
+                                {/* <div className='col-md-6' style={{paddingLeft: '0px'}}>
+                                    <Form.Check
+                                        defaultChecked
+                                        type={'checkbox'}
+                                        id={`ac}`}
+                                        label={`I want to avail`}
+                                    />
+                                </div> */}
+                                <input type="checkbox" defaultChecked name="vehicle2" value="service"/>
+                                <label for="vehicle2"> I want to avail <span>Insurance Service</span></label>
+                                {/* <div className='col-md-6'>Insurance Service</div> */}
                             </div>
-                            <div className='last_button'>SELECT & CONTINUE TO RETURN</div>
+                            {/* <div className='row last_button'> */}
+                                <button>SELECT & CONTINUE TO RETURN</button>
+                            {/* </div> */}
                         </div>
                     </div>
                     )}
